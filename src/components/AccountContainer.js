15a553f5b55e493e7606e16330bfe6837b101425
Xavier Carty <xaviercarty@gmail.com>
2020-05-20 11:03:56 -0400

    got the core deliverables done

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,79 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+  state = {
+    transactions: [],
+    searchTerm: ""
+
+  }
+
+  //Get Transactions
+  componentDidMount(){
+    fetch("http://localhost:6001/transactions")
+    .then(r => r.json())
+    .then(responseObj => {
+      console.log(responseObj)
+       this.setState({
+         transactions: responseObj
+       })  
+    })
+  }
+
+//Add Transcation
+  handleAdd = (newAdd) => {
+    
+    fetch("http://localhost:6001/transactions",{
+      method: "POST",
+      headers: {
+        "Content-Type": "Application/json"
+      },
+      body: JSON.stringify(newAdd)
+    })
+    .then(r => r.json())
+    .then(responseObj => {
+
+      let newList = [...this.state.transactions , newAdd]
+      this.setState({
+
+        transactions: newList
+
+      })
+    })
+
+  }
+
+  handleSearch = (searchTerm) => {
+  
+     this.setState({
+       
+         searchTerm,
+
+     })
+
+  }
+
+  //Seach functionality
+
+  renderTheSearch = () => {
+   const {searchTerm , transactions} = this.state
+   
+    let array = transactions.filter(transaction => {
+             
+        return ( transaction.description.toLowerCase().includes(searchTerm) 
+        || transaction.description.includes(searchTerm) )
+
+    })
+   
+    return array
+  }
+
+
   render() {
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search searchTerm={this.state.searchTerm} handleSearch={this.handleSearch}/>
+        <AddTransactionForm addList={this.handleAdd}/>
+        <TransactionsList transactions = {this.renderTheSearch()} />
       </div>
     );
   }
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,7 +1,12 @@
 import React from "react";
 import Transaction from "./Transaction";
 
-const TransactionsList = () => {
+const TransactionsList = (props) => {
+
+  const allTrans = props.transactions.map((trans ,index) => {
+    return (<Transaction key={index} transaction={trans} />)
+
+  })
   return (
     <table className="ui celled striped padded table">
       <tbody>
@@ -19,7 +24,7 @@ const TransactionsList = () => {
             <h3 className="ui center aligned header">Amount</h3>
           </th>
         </tr>
-        {/* render Transactions here */}
+        {allTrans}
       </tbody>
     </table>
   );
