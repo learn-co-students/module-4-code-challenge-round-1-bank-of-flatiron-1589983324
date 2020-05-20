28421c0956971cbee1950d984a86506ad3e89d84
StephanieZou1 <stephanie.zou3@gmail.com>
2020-05-20 11:08:27 -0400

    main deliverables

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,68 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+
+  state = {
+    allTransactions: [],
+    searchTerm: ""
+  }
+
+  handleSearchTerm = (termFromChild) => {
+    this.setState({
+      searchTerm: termFromChild
+    })
+  }
+
+  decideWhatArrayToRender = () => {
+    let copyOfAllTransactions = this.state.allTransactions.filter((transaction) => {
+      return transaction.description.toLowerCase().includes(this.state.searchTerm.toLowerCase())
+    })
+    return copyOfAllTransactions
+  }
+
+  componentDidMount(){
+    fetch(`http://localhost:6001/transactions`)
+      .then(response => response.json())
+      .then((fetchedTransactions) => {
+        this.setState({
+          allTransactions: fetchedTransactions
+        })
+      })
+  }
+
+  addNewTransaction = (newTransaction) => {
+    fetch(`http://localhost:6001/transactions`, {
+      method: "POST",
+      headers: {
+        'Content-type': 'Application/json'
+      },
+      body: JSON.stringify(newTransaction)
+    })
+      .then(response => response.json())
+      .then((newlyAddedTransaction) => {
+        let updatedTransactionsList = [...this.state.allTransactions, newlyAddedTransaction]
+        this.setState({
+          allTransactions: updatedTransactionsList
+        })
+      })
+  }
+
   render() {
+    console.log(this.state)
+    let sortedTransactions = this.decideWhatArrayToRender()
+    console.log(sortedTransactions, "SORTED TRANSACTIONS")
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search 
+          searchTerm={this.state.searchTerm}
+          handleSearchTerm={this.handleSearchTerm}
+        />
+        <AddTransactionForm 
+          addNewTransaction={this.addNewTransaction}
+        />
+        <TransactionsList 
+          transactions={this.decideWhatArrayToRender()}
+        />
       </div>
     );
   }
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,7 +1,17 @@
 import React from "react";
 import Transaction from "./Transaction";
 
-const TransactionsList = () => {
+const TransactionsList = (props) => {
+  
+  let arrayOfTransactions = props.transactions.map((singleTransaction) => {
+    return <Transaction 
+      transaction={singleTransaction}
+      key={singleTransaction.id}
+    />
+  })
+  
+  console.log(props)
+
   return (
     <table className="ui celled striped padded table">
       <tbody>
@@ -19,7 +29,7 @@ const TransactionsList = () => {
             <h3 className="ui center aligned header">Amount</h3>
           </th>
         </tr>
-        {/* render Transactions here */}
+        {arrayOfTransactions}
       </tbody>
     </table>
   );
