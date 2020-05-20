ef80d6d486189d7ee5c2696ef00ed8287dd929b3
isabel k. lee <hello@isabelklee.co>
2020-05-20 11:26:32 -0400

    created alphabetical sort for caetegories

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -7,7 +7,7 @@ class AccountContainer extends Component {
   state = {
     transactions: [],
     searchTerm: "",
-    selectValue: "all"
+    selectValue: "default"
   }
 
   componentDidMount() {
@@ -27,14 +27,36 @@ class AccountContainer extends Component {
   }
 
   pickTransactions = () => {
-    let { transactions, searchTerm } = this.state
+    let { transactions, searchTerm, selectValue } = this.state
+    let newArr = transactions
 
-    let newArr = transactions.filter((transaction) =>
+    newArr = transactions.filter((transaction) =>
       transaction.description.includes(searchTerm)
     )
+
+    if (selectValue === "default") {
+      newArr = transactions
+    } else if (selectValue === "category") {
+      newArr = transactions.sort(this.alphabeticalSort)
+      console.log("Sorted by category")
+    } else if (selectValue === "description") {
+      console.log("Sorted by description")
+    }
     return newArr
   }
 
+  alphabeticalSort = (a,b) => {
+    let valueA = a.category.toLowerCase()
+    let valueB = b.category.toLowerCase()
+    if (valueA < valueB) {
+      return -1
+    }
+    if (valueA > valueB) {
+      return 1
+    }
+    return 0
+  }
+  
   addTransaction = newTransaction => {
     let copyArr = this.state.transactions
 
@@ -66,8 +88,9 @@ class AccountContainer extends Component {
       <div>
         <Search searchTerm = { this.state.searchTerm } handleChange = {this.handleChange} />
         <AddTransactionForm addTransaction = {this.addTransaction} />
+        <label>Sort: </label>
         <select value = { this.state.selectValue } onChange = {this.handleSelectChange}>
-          <option value="all">All</option>
+          <option value="default">Default</option>
           <option value="category">Category A-Z</option>
           <option value="description">Description A-Z</option>
         </select>
